f474904d8eb4a1b8d4af2a071a7eac9a99549a4b
Jarom Rial <jarom.rial@gmail.com>
2022-10-12 15:43:56 -0600

    change colors to blue

--- resources/js/Components/Forms/SimpleSelect.vue
+++ resources/js/Components/Forms/SimpleSelect.vue
@@ -13,7 +13,7 @@ defineEmits(['update:modelValue']);
 <template>
     <Listbox as="div" v-model="modelValue" @update:model-value="$emit('update:modelValue', modelValue)">
         <div class="relative mt-1">
-            <ListboxButton class="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+            <ListboxButton class="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
                 <span class="block truncate">{{ modelValue.label }}</span>
                 <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                     <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -23,10 +23,10 @@ defineEmits(['update:modelValue']);
             <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
                 <ListboxOptions class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                     <ListboxOption as="template" v-for="(item, idx) in items" :key="idx" :value="item" v-slot="{ active, selected }">
-                        <li :class="[active ? 'text-white bg-indigo-600' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
+                        <li :class="[active ? 'text-white bg-blue-600' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
                             <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">{{ item.label }}</span>
 
-                            <span v-if="selected" :class="[active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
+                            <span v-if="selected" :class="[active ? 'text-white' : 'text-blue-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
                                 <CheckIcon class="h-5 w-5" aria-hidden="true" />
                             </span>
                         </li>
